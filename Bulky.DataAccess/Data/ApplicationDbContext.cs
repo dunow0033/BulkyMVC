3bce89e6dae82cadc840f1ee198c59761f3e5704
Brian Donnoe <dunow33@hotmail.com>
2024-05-10 19:27:16 -0400

    did some work adding Product, CategoryID foreign key, adding areas, and modifying the database

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,81 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Category> Categories { get; set; }
+		public DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().HasData(
+
+				new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+
+			modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Id = 1,
+					Title = "Cotton Candy",
+					Author = "Abby Muscles",
+					Description = "Praesent vitae sodales libero",
+					ISBN = "WS333333301",
+					ListPrice = 70,
+					Price = 65,
+					Price50 = 60,
+					Price100 = 55,
+					CategoryId = 1
+				},
+				new Product
+				{
+					Id = 2,
+					Title = "Rock in the Ocean",
+					Author = "Ron Parker",
+					Description = "Praesent vitae sodales libero",
+					ISBN = "WS33333330178787",
+					ListPrice = 30,
+					Price = 27,
+					Price50 = 25,
+					Price100 = 20,
+					CategoryId = 2
+				},
+				new Product
+				{
+					Id = 3,
+					Title = "Rock in the Ocean2",
+					Author = "Ron Parker2",
+					Description = "Praesent vitae sodales libero",
+					ISBN = "WS33333330173434",
+					ListPrice = 22,
+					Price = 34,
+					Price50 = 22,
+					Price100 = 21,
+					CategoryId = 1
+				},
+				new Product
+				{
+					Id = 4,
+					Title = "Rock in the Ocean3",
+					Author = "Ron Parker3",
+					Description = "Praesent vitae sodales libero",
+					ISBN = "WS33333330102398",
+					ListPrice = 18,
+					Price = 25,
+					Price50 = 19,
+					Price100 = 17,
+					CategoryId = 2
+				}
+			);
+		}
+    }
+}
---
+++ Bulky.DataAccess/Migrations/20240510213605_addProductsToDb.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductsToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Abby Muscles", "Praesent vitae sodales libero", "WS333333301", 70.0, 65.0, 55.0, 60.0, "Cotton Candy" },
+                    { 2, "Ron Parker", "Praesent vitae sodales libero", "WS33333330178787", 30.0, 27.0, 20.0, 25.0, "Rock in the Ocean" },
+                    { 3, "Ron Parker2", "Praesent vitae sodales libero", "WS33333330173434", 22.0, 34.0, 21.0, 22.0, "Rock in the Ocean2" },
+                    { 4, "Ron Parker3", "Praesent vitae sodales libero", "WS33333330102398", 18.0, 25.0, 17.0, 19.0, "Rock in the Ocean3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
